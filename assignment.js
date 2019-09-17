10c3c3b7856342147e8cf8e3611e272864582cda
Ociel Coria <Oc102892@gmail.com>
2019-09-16 17:01:05 -0700

    finalized the homework

--- assignment.js
+++ assignment.js
@@ -31,7 +31,11 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 1:')
 
 // Add your code below this line
+const baseNumber = 2;
 
+for (i = 0; i <= 12; i++) {
+    console.log(baseNumber * i)
+}
 // Add your code above this line
 
 console.log('')
@@ -65,6 +69,13 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 2:')
 
 // Add your code below this line
+let numbers = [3, 48, 29, 12, 18]
+
+let currentLargestNumber = numbers.reduce(function (a, b) {
+    return Math.max(a, b)
+});
+
+console.log(currentLargestNumber)
 
 // Add your code above this line
 
@@ -112,7 +123,44 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 3:')
 
 // Add your code below this line
+let userChoice = prompt("Which one do you choose, rock, paper, or scissors?")
+let computerChoice = Math.random()
+if (computerChoice < 0.34) {
+    computerChoice = "rock"
+} else if (computerChoice <= 0.67) {
+    computerChoice = "paper"
+} else {
+    computerChoice = "scissors"
+}
 
+let compare = function (player1Choice, player2Choice) {
+    if (player1Choice === player2Choice) {
+        return "the result is a tie"
+    }
+    if (player1Choice === "rock") {
+        if (player2Choice === "scissors") {
+            return "rock wins"
+        } else {
+            return "paper wins"
+        }
+    }
+    if (player1Choice === "paper") {
+        if (player2Choice === "rock") {
+            return "paper wins"
+        } else {
+            return "scissors wins"
+        }
+    }
+    if (player1Choice === "scissors") {
+        if (player2Choice === "rock") {
+            return "rock wins"
+        } else {
+            return "scissors wins"
+        }
+    }
+};
+
+console.log(compare(userChoice, computerChoice));
 // Add your code above this line
 
 console.log('')
