bd68c39cc68c7619df481e731054ce593c7da274
Michael Robinson <michael.robinson@mac.com>
2019-09-18 12:12:44 -0700

    homework

--- assignment.js
+++ assignment.js
@@ -32,6 +32,10 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+for (let i = 0; i < 25; i = i+2) {
+  console.log(i)
+}
+
 // Add your code above this line
 
 console.log('')
@@ -65,6 +69,15 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 2:')
 
 // Add your code below this line
+
+const array1 = [57, 13, 4, 38]
+
+console.log(Math.max(...array1));
+
+const array2 = [3, 48, 29, 12, 18]
+
+console.log(Math.max(...array2));
+
 
 // Add your code above this line
 
@@ -112,6 +125,60 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 3:')
 
 // Add your code below this line
+
+let playerAScore = 0
+let playerBScore = 0
+let tieScore = 0
+const choices = ["rock","paper","scissors"]
+
+for (let i = 0; i < 30; i++) {
+
+    var playerAChoice = choices[Math.floor(Math.random()*choices.length)]
+    var playerBChoice = choices[Math.floor(Math.random()*choices.length)]
+
+
+      if (playerAChoice === playerBChoice ) {
+          console.log("tie")
+          tieScore++
+      }
+
+      if ((playerAChoice === "rock") && (playerBChoice === "scissors")) {
+          console.log("playerA wins")
+          playerAScore++
+      }
+
+      if ((playerAChoice === "scissors") && (playerBChoice === "rock")) {
+          console.log("playerB wins")
+          playerBScore++
+      }
+
+      if ((playerAChoice === "paper") && (playerBChoice === "rock")) {
+          console.log("playerA wins")
+          playerAScore++
+      }
+
+      if ((playerAChoice === "rock") && (playerBChoice === "paper")) {
+          console.log("playerB wins")
+          playerBScore++
+      }
+
+      if ((playerAChoice === "paper") && (playerBChoice === "scissors")) {
+          console.log("playerB wins")
+          playerBScore++
+      }
+
+      if ((playerAChoice === "scissors") && (playerBChoice === "paper")) {
+          console.log("playerA wins")
+          playerAScore++
+        }
+      console.log("==================")
+}
+
+console.log(`playerA Score: ${playerAScore}`)
+console.log(`playerB Score: ${playerBScore}`)
+console.log(`# of Tie Scores: ${tieScore}`)
+
+
 
 // Add your code above this line
 
