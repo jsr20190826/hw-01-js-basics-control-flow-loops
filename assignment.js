a55c2bc669fe7d13818372f505a999a5a864a4be
John Tillman <jct2692@gmail.com>
2019-09-18 00:28:09 -0400

    Update assignment.js
    Completed HW

--- assignment.js
+++ assignment.js
@@ -29,8 +29,21 @@ Use the following test cases to confirm your program meets the success criteria
     24
 ************/
 console.log('Problem 1:')
-
+console.log('')
 // Add your code below this line
+ let baseNumber = 2
+
+
+ // loop to run 12 multiples of the baseNumber
+ for (let i = 1; i < 13; i++){
+  let num = baseNumber * i
+  console.log(num)
+  }
+
+
+
+
+
 
 // Add your code above this line
 
@@ -63,8 +76,28 @@ Use the following test cases to confirm your program meets the success criteria
 ************/
 
 console.log('Problem 2:')
-
+console.log('')
 // Add your code below this line
+
+const numbers = [57, 13, 4, 38]
+const numbers1 = [3, 48, 29, 12, 18]
+
+//finds highest number in the array
+let currentLargestNumber = numbers.reduce(function(a, b) {
+    return Math.max(a, b);
+});
+let currentLargestNumber1 = numbers1.reduce(function(a, b) {
+    return Math.max(a, b);
+});
+
+
+
+console.log(currentLargestNumber)
+console.log(currentLargestNumber1)
+
+
+
+
 
 // Add your code above this line
 
@@ -112,6 +145,74 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 3:')
 
 // Add your code below this line
+console.log('')
+let playerAChoice = Math.random()
+let playerBChoice = Math.random()
+
+
+console.log(playerAChoice)
+//generates playerA's decsion
+ if (playerAChoice <0.333){
+     playerAChoice = "rock"
+ }else if(playerAChoice <=0.666){
+     playerAChoice = "paper"
+ }
+ else{
+     playerAChoice = "scissors"
+ }
+
+
+//generates playerB's decsion
+if (playerBChoice <0.333){
+    playerBChoice = "rock"
+}else if(playerBChoice <=0.666){
+    playerBChoice = "paper"
+}
+else{
+    playerBChoice = "scissors"
+}
+
+
+console.log(`Player One chooses ${playerAChoice} and Player Two chooses ${playerBChoice}.`)
+
+
+
+//function for winning conditions
+let compare = function(choice1,choice2){
+    if(choice1===choice2){
+        return "The result is a tie!";
+    }
+    if(choice1==="rock"){
+        if(choice2==="scissors"){
+            return "Player One wins"
+        }
+        else{
+            return "Player Two wins";
+        }
+    }
+    if(choice1==="paper"){
+        if(choice2==="rock"){
+            return "Player One wins";
+        }
+        else{
+            return "Player Two wins";
+        }
+    }
+    if(choice1==="scissors"){
+        if(choice2==="rock"){
+            return "Player Two wins";
+        }
+        else{
+            return "Player One wins";
+        }
+    }
+};
+console.log('')
+// calls compare function with the players choice
+console.log(compare(playerAChoice,playerBChoice))
+
+
+
 
 // Add your code above this line
 
